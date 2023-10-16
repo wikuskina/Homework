d6f84b21f50114e08b619b3c3c49f229730133b1
wikuskina <wikuskina@gmail.com>
2023-10-16 01:18:38 +0100

    Commit - HashMap

--- itgirl/src/main/java/ru/itgirl/WeekdaysController.java
+++ itgirl/src/main/java/ru/itgirl/WeekdaysController.java
@@ -4,10 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class WeekdaysController {
-    @GetMapping("/hello")
-public String hello(@RequestParam(value = "day", defaultValue = "День") String day) {
-    return String.format("Сегодня %s!", day);
-}
-}
+
+    @GetMapping("/dayWeek")
+    public String dayOf(@RequestParam(value = "day") String dayString) {
+        HashMap<String, String> DaysList = new HashMap<String, String>();
+        DaysList.put("MONDAY", "Понедельник");
+        DaysList.put("TUESDAY", "Вторник");
+        DaysList.put("WEDNESDAY", "Среда");
+        DaysList.put("THURSDAY", "Четверг");
+        DaysList.put("FRIDAY", "Пятница");
+        DaysList.put("SATURDAY", "Суббота");
+        DaysList.put("SUNDAY", "Воскресенье");
+
+        for (HashMap.Entry<String, String> day: DaysList.entrySet()) {
+            if (day.getKey().equals(dayString)){
+                return String.format("Сегодня %s!", day.getValue());
+            }
+        }
+        return "";
+    }
+    }
